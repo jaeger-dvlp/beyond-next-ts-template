7596c1d9f91dea266dab6c03aaba455059c5764a
jaeger-dvlp <omer.kayalar0101@icloud.com>
2026-02-18 18:16:53 +0300

    lint

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const subPathPrefix = process.env.NEXT_PUBLIC_APP_SUBPATH_PREFIX || '';
 
 module.exports = {
   reactStrictMode: true,
+  allowedDevOrigins: ['http://localhost:3000'],
   basePath: subPathPrefix,
 };
